191704b89a01d6f7f93ffa5e7320cc580759c363
pycat <kkampardi@gmail.com>
2019-06-24 11:09:29 +0300

    Fix urls

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 
+
 urlpatterns = [
-    url(r'^product/(?P<slug>[\w-]+)$', 'products.views.product_detail', name='product_detail'),
-    url(r'^product/(?P<slug>[\w-]+)/edit$', 'products.views.product_edit', name='product_edit'),
-    url(r'^products$', 'products.views.product_list', name='product_list'),
-    url(r'^product/create$', 'products.views.product_create', name='product_create'),
+    url(r'^product/new/$', 'products.views.product_create', name='product_create'),
+    url(r'^product/(?P<slug>[\w-]+)/$', 'products.views.product_detail', name='product_detail'),
+    url(r'^product/(?P<slug>[\w-]+)/edit/$', 'products.views.product_edit', name='product_edit'),
+    url(r'^products/$', 'products.views.product_list', name='product_list'),
 ]
